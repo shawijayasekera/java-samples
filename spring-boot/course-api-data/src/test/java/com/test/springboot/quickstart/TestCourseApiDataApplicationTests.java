98ffbf335a22600c3fe8127656013d7abd5c6258
shawijayasekera <shashika@wso2telco.com>
2019-11-22 11:50:10 +0530

    Add course-api-data project

---
+++ spring-boot/course-api-data/src/test/java/com/test/springboot/quickstart/TestCourseApiDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.springboot.quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCourseApiDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
