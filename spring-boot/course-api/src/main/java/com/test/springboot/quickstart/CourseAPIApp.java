1d5e453306cd4d0baf788fa4204167df84aceb47
shawijayasekera <shashika@wso2telco.com>
2019-11-22 11:49:22 +0530

    Add course-api project

---
+++ spring-boot/course-api/src/main/java/com/test/springboot/quickstart/CourseAPIApp.java
@@ -0,0 +1,13 @@
+package com.test.springboot.quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAPIApp {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CourseAPIApp.class, args);
+	}
+}
