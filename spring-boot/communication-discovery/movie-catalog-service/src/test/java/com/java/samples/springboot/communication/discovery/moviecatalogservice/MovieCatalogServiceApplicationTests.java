fbd83f6f9a217d9b01b890ab1030aa56838658a9
shawijayasekera <shashika@wso2telco.com>
2019-12-08 19:00:45 +0530

    Add projects related to the communication-discovery

---
+++ spring-boot/communication-discovery/movie-catalog-service/src/test/java/com/java/samples/springboot/communication/discovery/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.samples.springboot.communication.discovery.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-boot/communication-discovery/ratings-data-service/src/test/java/com/java/samples/springboot/communication/discovery/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.samples.springboot.communication.discovery.ratingsdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
