fc2d037c08c2e714ab9086836ddfdf5e163fb769
shawijayasekera <shashika@wso2telco.com>
2019-12-11 17:36:51 +0530

    Implement ratings data service

---
+++ spring-boot/communication-discovery/ratings-data-service/src/main/java/com/java/samples/springboot/communication/discovery/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,17 @@
+package com.java.samples.springboot.communication.discovery.ratingsdataservice.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.java.samples.springboot.communication.discovery.ratingsdataservice.models.Rating;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+	@RequestMapping("/{movieId}")
+	public Rating getRating(@PathVariable("movieId") String movieId) {
+
+		return new Rating(movieId, 4);
+	}
+}
