9af9b353a9b45dce58474aa340fe1d70f3e2b493
shawijayasekera <shashika@wso2telco.com>
2019-12-19 23:18:04 +0530

    Add @HystrixProperty to change the hystrix default parameter values

--- spring-boot/fault-tolerance-resilience/movie-catalog-service/src/main/java/com/java/samples/springboot/faulttolerance/resilience/moviecatalogservice/services/UserRatingInfo.java
+++ spring-boot/fault-tolerance-resilience/movie-catalog-service/src/main/java/com/java/samples/springboot/faulttolerance/resilience/moviecatalogservice/services/UserRatingInfo.java
@@ -7,6 +7,7 @@ import org.springframework.web.client.RestTemplate;
 import com.java.samples.springboot.faulttolerance.resilience.moviecatalogservice.models.Rating;
 import com.java.samples.springboot.faulttolerance.resilience.moviecatalogservice.models.UserRating;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;;
 
 @Service
 public class UserRatingInfo {
@@ -14,7 +15,12 @@ public class UserRatingInfo {
 	@Autowired
 	private RestTemplate restTemplate;
 	
-	@HystrixCommand(fallbackMethod = "getFallbackUserRating")
+	@HystrixCommand(fallbackMethod = "getFallbackUserRating", commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"), // timeout value in seconds
+			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"), // no of last requests
+			@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"), // failure percentage of last no of requests
+			@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000") // how many seconds to not send the request to endpoint
+	})
 	public UserRating getUserRating(String userId) {
 
 		return restTemplate.getForObject("http://RATINGS-DATA-SERVICE/ratingsdata/users/" + userId, UserRating.class);
