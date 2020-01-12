f8ed5d5092bea9d4b94e313d2f88ebfed17e2671
shawijayasekera <shashika@wso2telco.com>
2020-01-12 17:01:12 +0530

    Add spring-boot-security project

---
+++ spring-boot/security/spring-boot-security/src/test/java/com/java/samples/springboot/security/springbootsecurity/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.samples.springboot.security.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
