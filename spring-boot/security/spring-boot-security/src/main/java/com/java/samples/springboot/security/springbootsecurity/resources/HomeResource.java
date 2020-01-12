e9c5093b17aed1cecb23c1fc9b8294ee6169c1c1
shawijayasekera <shashika@wso2telco.com>
2020-01-12 19:36:30 +0530

    Add changes to login using username and password configured in application.properties file

---
+++ spring-boot/security/spring-boot-security/src/main/java/com/java/samples/springboot/security/springbootsecurity/resources/HomeResource.java
@@ -0,0 +1,14 @@
+package com.java.samples.springboot.security.springbootsecurity.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+	@GetMapping("/")
+	public String home() {
+
+		return ("<h1>Welcome</h1>");
+	}
+}
