d552666a0a715c556b9de5587caf3317d5ebe953
shawijayasekera <shashika@wso2telco.com>
2020-06-06 00:53:03 +0530

    Move data initialization part to the schema.sql and data.sql files.

--- spring-boot/security/spring-boot-security-jdbc/src/main/java/com/java/samples/springboot/security/springbootsecurityjdbc/security/SecurityConfiguration.java
+++ spring-boot/security/spring-boot-security-jdbc/src/main/java/com/java/samples/springboot/security/springbootsecurityjdbc/security/SecurityConfiguration.java
@@ -1,13 +1,11 @@
 package com.java.samples.springboot.security.springbootsecurityjdbc.security;
 
 import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -20,16 +18,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 		// Set your configuration on the auth object
 		auth.jdbcAuthentication()
-			.dataSource(dataSource) // for the embedded databases spring will automatically creates a datasource
-			.withDefaultSchema() // with this spring will creates default user and authority tables automatically
-			.withUser(User.withUsername("user") // add pre-defined user details
-						.password("{noop}user") // with {noop} passwords will store as clear text
-						.roles("USER")
-					)
-			.withUser(User.withUsername("admin")
-					.password("{noop}admin")
-					.roles("ADMIN")
-				);
+			.dataSource(dataSource); // for the embedded databases spring will automatically creates a datasource		
 	}
 
 	@Override
---
+++ spring-boot/security/spring-boot-security-jdbc/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (username, password, enabled)
+	VALUES ('user', '{noop}user', true);
+	
+INSERT INTO users (username, password, enabled)
+	VALUES ('admin', '{noop}admin', true);
+	
+INSERT INTO authorities (username, authority)
+	VALUES ('user', 'ROLE_USER');
+	
+INSERT INTO authorities (username, authority)
+	VALUES ('admin', 'ROLE_ADMIN');
