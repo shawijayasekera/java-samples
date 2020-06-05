939a7af01ae3fc7a28422b824334939e0aea0d9a
shawijayasekera <shashika@wso2telco.com>
2020-06-06 00:20:46 +0530

    Create pre define users with default schema

---
+++ spring-boot/security/spring-boot-security-jdbc/src/main/java/com/java/samples/springboot/security/springbootsecurityjdbc/security/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.java.samples.springboot.security.springbootsecurityjdbc.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	DataSource dataSource;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		// Set your configuration on the auth object
+		auth.jdbcAuthentication()
+			.dataSource(dataSource) // for the embedded databases spring will automatically creates a datasource
+			.withDefaultSchema() // with this spring will creates default user and authority tables automatically
+			.withUser(User.withUsername("user") // add pre-defined user details
+						.password("{noop}user") // with {noop} passwords will store as clear text
+						.roles("USER")
+					)
+			.withUser(User.withUsername("admin")
+					.password("{noop}admin")
+					.roles("ADMIN")
+				);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		// Need to configure from most restricted to least restricted
+		http.authorizeRequests()
+			.antMatchers("/admin").hasRole("ADMIN") // only users with ADMIN role can access this page
+			.antMatchers("/user").hasAnyRole("ADMIN", "USER") // only users with USER role can access this page
+			.antMatchers("/").permitAll() // root URL - this means all can access the home page
+			.and()
+			.formLogin(); // this means form based login
+	}
+}
