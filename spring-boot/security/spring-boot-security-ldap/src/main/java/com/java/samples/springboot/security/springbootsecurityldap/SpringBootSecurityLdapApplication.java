85d06f4f239159eb19e22b187bb7b8aea4a8a3b0
shawijayasekera <shashika@wso2telco.com>
2020-06-07 01:06:31 +0530

    Add spring-boot-security-ldap application

---
+++ spring-boot/security/spring-boot-security-ldap/src/main/java/com/java/samples/springboot/security/springbootsecurityldap/SpringBootSecurityLdapApplication.java
@@ -0,0 +1,13 @@
+package com.java.samples.springboot.security.springbootsecurityldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityLdapApplication.class, args);
+	}
+
+}
---
+++ spring-boot/security/spring-boot-security-ldap/src/test/java/com/java/samples/springboot/security/springbootsecurityldap/SpringBootSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.samples.springboot.security.springbootsecurityldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
