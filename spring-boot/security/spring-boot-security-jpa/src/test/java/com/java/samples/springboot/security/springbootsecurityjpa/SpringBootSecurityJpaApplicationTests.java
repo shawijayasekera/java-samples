db87e0d45444e3d74c41cf4701054cb6b575041c
shawijayasekera <shashika@wso2telco.com>
2020-06-06 19:12:30 +0530

    Add spring-boot-security-jpa application

---
+++ spring-boot/security/spring-boot-security-jpa/src/test/java/com/java/samples/springboot/security/springbootsecurityjpa/SpringBootSecurityJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.samples.springboot.security.springbootsecurityjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
