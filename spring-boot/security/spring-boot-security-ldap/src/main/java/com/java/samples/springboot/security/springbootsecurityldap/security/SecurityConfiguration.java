e7347d771afbe97127d6bc43e9c5e888f4a72422
shawijayasekera <shashika@wso2telco.com>
2020-06-07 16:17:12 +0530

    Remove unused object

--- spring-boot/security/spring-boot-security-ldap/src/main/java/com/java/samples/springboot/security/springbootsecurityldap/security/SecurityConfiguration.java
+++ spring-boot/security/spring-boot-security-ldap/src/main/java/com/java/samples/springboot/security/springbootsecurityldap/security/SecurityConfiguration.java
@@ -1,18 +1,13 @@
 package com.java.samples.springboot.security.springbootsecurityldap.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity // tells spring security to this is a configuration class
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-	@Autowired
-	UserDetailsService userDetailsService;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
