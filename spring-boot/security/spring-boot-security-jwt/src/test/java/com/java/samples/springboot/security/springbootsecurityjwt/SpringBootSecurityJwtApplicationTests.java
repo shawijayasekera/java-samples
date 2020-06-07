d733be2d0d610d73cfcf9878215b53a931366e11
shawijayasekera <shashika@wso2telco.com>
2020-06-07 14:41:35 +0530

    Add spring-boot-security-jwt application

---
+++ spring-boot/security/spring-boot-security-jwt/src/test/java/com/java/samples/springboot/security/springbootsecurityjwt/SpringBootSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.samples.springboot.security.springbootsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
