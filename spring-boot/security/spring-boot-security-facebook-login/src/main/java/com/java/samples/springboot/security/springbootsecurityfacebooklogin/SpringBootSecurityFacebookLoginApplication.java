92aaba155a116f2c828f6ced8f8dc0d3bd3fc21b
shawijayasekera <shashika@wso2telco.com>
2020-06-14 01:34:33 +0530

    Add spring-boot-security-facebook-login application

---
+++ spring-boot/security/spring-boot-security-facebook-login/src/main/java/com/java/samples/springboot/security/springbootsecurityfacebooklogin/SpringBootSecurityFacebookLoginApplication.java
@@ -0,0 +1,13 @@
+package com.java.samples.springboot.security.springbootsecurityfacebooklogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityFacebookLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityFacebookLoginApplication.class, args);
+	}
+
+}
---
+++ spring-boot/security/spring-boot-security-facebook-login/src/test/java/com/java/samples/springboot/security/springbootsecurityfacebooklogin/SpringBootSecurityFacebookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.samples.springboot.security.springbootsecurityfacebooklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityFacebookLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
