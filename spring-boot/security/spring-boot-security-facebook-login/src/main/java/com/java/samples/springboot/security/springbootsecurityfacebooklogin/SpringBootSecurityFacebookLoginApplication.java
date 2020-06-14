cbdf355d40da7c8c460ec06c963cd7cdd7ddbb4a
shawijayasekera <shashika@wso2telco.com>
2020-06-14 14:40:14 +0530

    Implement login with facebook

--- spring-boot/security/spring-boot-security-facebook-login/src/main/java/com/java/samples/springboot/security/springbootsecurityfacebooklogin/SpringBootSecurityFacebookLoginApplication.java
+++ spring-boot/security/spring-boot-security-facebook-login/src/main/java/com/java/samples/springboot/security/springbootsecurityfacebooklogin/SpringBootSecurityFacebookLoginApplication.java
@@ -2,8 +2,10 @@ package com.java.samples.springboot.security.springbootsecurityfacebooklogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso // instruct springboot to do the oauth based authentication 
 public class SpringBootSecurityFacebookLoginApplication {
 
 	public static void main(String[] args) {
