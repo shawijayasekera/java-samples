4f0e29f6e7cc0e608a631f4be27eac8b0be7d881
shawijayasekera <shashika@wso2telco.com>
2020-06-14 15:02:40 +0530

    Add spring-boot-security-custom-login application

---
+++ spring-boot/security/spring-boot-security-custom-login/src/main/java/com/java/samples/springboot/security/springbootsecuritycustomlogin/SpringBootSecurityCustomLoginApplication.java
@@ -0,0 +1,13 @@
+package com.java.samples.springboot.security.springbootsecuritycustomlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityCustomLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityCustomLoginApplication.class, args);
+	}
+
+}
---
+++ spring-boot/security/spring-boot-security-custom-login/src/test/java/com/java/samples/springboot/security/springbootsecuritycustomlogin/SpringBootSecurityCustomLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.samples.springboot.security.springbootsecuritycustomlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityCustomLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
