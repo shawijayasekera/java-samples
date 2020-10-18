a5115769c8a5c3b1d98a7c4546b886cff897bc08
Shashika Wijayasekera <shashika.wijayasekera@hotmail.com>
2020-10-18 18:32:48 +0530

    Add accessing-data-mongodb-rest project

---
+++ spring-boot/spring-mongodb/accessing-data-mongodb-rest/src/main/java/com/test/springboot/springdatamongodb/accessingdatamongodbrest/AccessingDataMongoDbWithRestApplication.java
@@ -0,0 +1,13 @@
+package com.test.springboot.springdatamongodb.accessingdatamongodbrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataMongoDbWithRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataMongoDbWithRestApplication.class, args);
+	}
+
+}
---
+++ spring-boot/spring-mongodb/accessing-data-mongodb-rest/src/test/java/com/test/springboot/springdatamongodb/accessingdatamongodbrest/AccessingDataMongoDbWithRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.springboot.springdatamongodb.accessingdatamongodbrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataMongoDbWithRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
