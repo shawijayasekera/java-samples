14041f811b9d524c88be01363336fdedde6f8b85
Shashika Wijayasekera <shashika.wijayasekera@hotmail.com>
2020-10-26 20:49:17 +0530

    Add struts.xml file and configure to display the success.jsp

---
+++ struts/struts-2-basic/struts-2-starter/src/main/java/com/test/struts/struts2basic/struts2starter/action/TutorialAction.java
@@ -0,0 +1,10 @@
+package com.test.struts.struts2basic.struts2starter.action;
+
+public class TutorialAction {
+
+	public String execute() {
+
+		System.out.println("Hello Execute");
+		return "success";
+	}
+}
