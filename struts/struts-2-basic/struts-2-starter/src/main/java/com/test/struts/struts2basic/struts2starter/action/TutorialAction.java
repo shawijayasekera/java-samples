bd8536c0111a73bd3377a38fe122b6a43c1d8bd7
Shashika Wijayasekera <shashika.wijayasekera@hotmail.com>
2020-10-28 19:14:09 +0530

    Add business service

--- struts/struts-2-basic/struts-2-starter/src/main/java/com/test/struts/struts2basic/struts2starter/action/TutorialAction.java
+++ struts/struts-2-basic/struts-2-starter/src/main/java/com/test/struts/struts2basic/struts2starter/action/TutorialAction.java
@@ -1,10 +1,14 @@
 package com.test.struts.struts2basic.struts2starter.action;
 
+import com.test.struts.struts2basic.struts2starter.service.TutorialFinderService;
+
 public class TutorialAction {
 
 	public String execute() {
 
-		System.out.println("Hello Execute");
+		TutorialFinderService tutorialFinderService = new TutorialFinderService();
+		String bestTutorialSite = tutorialFinderService.getBestTutorialSite();
+		System.out.println("Best Tutorial Site: " + bestTutorialSite);
 		return "success";
 	}
 }
---
+++ struts/struts-2-basic/struts-2-starter/src/main/java/com/test/struts/struts2basic/struts2starter/service/TutorialFinderService.java
@@ -0,0 +1,9 @@
+package com.test.struts.struts2basic.struts2starter.service;
+
+public class TutorialFinderService {
+
+	public String getBestTutorialSite() {
+		
+		return "Java Website";
+	}
+}
