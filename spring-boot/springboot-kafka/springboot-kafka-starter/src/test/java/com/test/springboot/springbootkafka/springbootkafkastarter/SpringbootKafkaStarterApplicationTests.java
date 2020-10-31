89ee01ea9f20a17ecb48e5aa82f78cab7339f348
Shashika Wijayasekera <shashika.wijayasekera@hotmail.com>
2020-10-31 16:22:53 +0530

    Add springboot-kafka-starter project.

---
+++ spring-boot/springboot-kafka/springboot-kafka-starter/src/test/java/com/test/springboot/springbootkafka/springbootkafkastarter/SpringbootKafkaStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.springboot.springbootkafka.springbootkafkastarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
