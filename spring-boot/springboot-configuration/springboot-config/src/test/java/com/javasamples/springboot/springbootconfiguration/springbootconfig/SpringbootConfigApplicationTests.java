43fb9578a9742fcbcdb620ef6ba54df112c399f4
Shashika Wijayasekera <shashika.wijayasekera@hotmail.com>
2020-11-21 20:42:36 +0530

    Add springboot-config project

---
+++ spring-boot/springboot-configuration/springboot-config/src/test/java/com/javasamples/springboot/springbootconfiguration/springbootconfig/SpringbootConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javasamples.springboot.springbootconfiguration.springbootconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
