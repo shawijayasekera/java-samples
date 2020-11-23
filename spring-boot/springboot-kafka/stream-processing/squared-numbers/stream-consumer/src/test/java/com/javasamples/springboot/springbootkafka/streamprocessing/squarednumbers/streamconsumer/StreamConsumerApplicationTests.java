c4028fee48344d9fd063a628d9ddd4f0be9661a4
Shashika Wijayasekera <shashika.wijayasekera@hotmail.com>
2020-11-23 23:03:50 +0530

    Add stream-consumer project.

---
+++ spring-boot/springboot-kafka/stream-processing/squared-numbers/stream-consumer/src/test/java/com/javasamples/springboot/springbootkafka/streamprocessing/squarednumbers/streamconsumer/StreamConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javasamples.springboot.springbootkafka.streamprocessing.squarednumbers.streamconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
