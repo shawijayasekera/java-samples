ec1de35bd7f885c8675128461d76b641812a9575
Shashika Wijayasekera <shashika.wijayasekera@hotmail.com>
2020-11-23 22:49:39 +0530

    Add stream-producer project

---
+++ spring-boot/springboot-kafka/stream-processing/squared-numbers/stream-producer/src/main/java/com/javasamples/springboot/springbootkafka/streamprocessing/squarednumbers/streamproducer/StreamProducerApplication.java
@@ -0,0 +1,13 @@
+package com.javasamples.springboot.springbootkafka.streamprocessing.squarednumbers.streamproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamProducerApplication.class, args);
+	}
+
+}
