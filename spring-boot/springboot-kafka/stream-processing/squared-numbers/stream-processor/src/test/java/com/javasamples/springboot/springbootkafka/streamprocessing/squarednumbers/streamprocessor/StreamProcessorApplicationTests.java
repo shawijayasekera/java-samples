5bcd869a45d74a82b4bf6311a80f2488439e96f1
Shashika Wijayasekera <shashika.wijayasekera@hotmail.com>
2020-11-23 23:21:47 +0530

    Add stream-processor project.

---
+++ spring-boot/springboot-kafka/stream-processing/squared-numbers/stream-processor/src/test/java/com/javasamples/springboot/springbootkafka/streamprocessing/squarednumbers/streamprocessor/StreamProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javasamples.springboot.springbootkafka.streamprocessing.squarednumbers.streamprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
