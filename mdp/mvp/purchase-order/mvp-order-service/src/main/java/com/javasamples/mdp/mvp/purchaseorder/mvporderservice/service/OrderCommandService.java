8c6aefd11458ca92469d0b3fba9f7b42d0f271a5
Shashika Wijayasekera <shashika.wijayasekera@hotmail.com>
2020-11-25 00:54:04 +0530

    Remove some unrelated classes and interfaces for the mvp.

--- mdp/mvp/purchase-order/mvp-order-service/src/main/java/com/javasamples/mdp/mvp/purchaseorder/mvporderservice/service/OrderCommandService.java
+++
@@ -1,7 +0,0 @@
-package com.javasamples.mdp.mvp.purchaseorder.mvporderservice.service;
-
-public interface OrderCommandService {
-
-	void createOrder(int userIndex, int productIndex);
-    void cancelOrder(long orderId);
-}
