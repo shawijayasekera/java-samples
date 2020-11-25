ccb48a5b34510ace320793068dfd32c404525bfa
Shashika Wijayasekera <shashika.wijayasekera@hotmail.com>
2020-11-25 23:06:49 +0530

    Remove order-service project.

--- mdp/cqrs/purchase-order/order-service/src/main/java/com/javasamples/mdp/cqrs/purchaseorder/orderservice/service/OrderCommandService.java
+++
@@ -1,7 +0,0 @@
-package com.javasamples.mdp.cqrs.purchaseorder.orderservice.service;
-
-public interface OrderCommandService {
-
-	void createOrder(int userIndex, int productIndex);
-    void cancelOrder(long orderId);
-}
