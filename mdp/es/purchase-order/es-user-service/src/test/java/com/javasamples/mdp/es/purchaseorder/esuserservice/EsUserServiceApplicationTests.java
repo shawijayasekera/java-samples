f6999228342c3c52f8410ea3143ad352e297e4b1
Shashika Wijayasekera <shashika.wijayasekera@hotmail.com>
2020-12-06 14:51:26 +0530

    Add es-user-service project.

---
+++ mdp/es/purchase-order/es-user-service/src/test/java/com/javasamples/mdp/es/purchaseorder/esuserservice/EsUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javasamples.mdp.es.purchaseorder.esuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
