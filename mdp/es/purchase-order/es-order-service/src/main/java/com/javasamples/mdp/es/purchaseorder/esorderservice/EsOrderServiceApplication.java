ce672855f5288b3f6c091a7ff40d873ea8d8335b
Shashika Wijayasekera <shashika.wijayasekera@hotmail.com>
2020-12-06 14:52:04 +0530

    Add es-order-service project.

---
+++ mdp/es/purchase-order/es-order-service/src/main/java/com/javasamples/mdp/es/purchaseorder/esorderservice/EsOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javasamples.mdp.es.purchaseorder.esorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsOrderServiceApplication.class, args);
+	}
+
+}
---
+++ mdp/es/purchase-order/es-order-service/src/test/java/com/javasamples/mdp/es/purchaseorder/esorderservice/EsOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javasamples.mdp.es.purchaseorder.esorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
